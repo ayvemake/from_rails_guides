7f4e7fc09adc2da7f4d63c2ad4d6c873cc6a9c7e
ayvemake <anilcan.kahraman@gmail.com>
2024-12-18 17:14:51 +0100

    Basic manipulations, create update delete items

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/products", to: "products#index"
   post "/products", to: "products#create"
+  root "products#index"
+  resources :products
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
