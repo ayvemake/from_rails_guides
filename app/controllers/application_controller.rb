c0bf03ad69a402cd047c25904bb8e12f88e8dbf2
ayvemake <anilcan.kahraman@gmail.com>
2024-12-18 17:38:34 +0100

    allow consumers to enter in the website, update and add restrictions, only landing page credits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include Authentication
+  allow_unauthenticated_access only: %i[ index show ]
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
